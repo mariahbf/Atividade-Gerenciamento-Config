d8739bd4124bd8cfafd89b4aa3986c4501bac68f
Mariah Freire <mahbackesf@gmail.com>
2023-08-29 11:04:04 -0300

    Mariah

--- CadastroPessoa.java
+++ CadastroPessoa.java
@@ -1,2 +1,79 @@
-package PACKAGE_NAME;public class CadastroPessoa {
+import java.util.*;
+public class CadastroPessoa {
+    private static final List<Pessoa> pessoas = new ArrayList<>();
+    private static final Map<String, Pessoa> cpfToPessoaMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar pessoa");
+            System.out.println("2 - Consultar por CPF");
+            System.out.println("3 - Listar todas as pessoas cadastradas");
+            System.out.println("4 - Sair");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1 -> cadastrarPessoa(scanner);
+                case 2 -> consultarPorCpf(scanner);
+                case 3 -> listarPessoasCadastradas();
+                case 4 -> {
+                    System.out.println("Encerrando o programa.");
+                    scanner.close();
+                    return;
+                }
+                default -> System.out.println("Opção inválida. Escolha uma opção válida.");
+            }
+        }
+    }
+
+    private static void cadastrarPessoa(Scanner scanner) {
+        System.out.println("Digite o nome da pessoa:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o CPF da pessoa:");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Digite a idade da pessoa:");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o gênero da pessoa:");
+        String genero = scanner.nextLine();
+
+        Pessoa pessoa = new Pessoa(nome, cpf, idade, genero);
+        pessoas.add(pessoa);
+        cpfToPessoaMap.put(cpf, pessoa);
+
+        System.out.println("Pessoa cadastrada com sucesso!");
+    }
+
+    private static void consultarPorCpf(Scanner scanner) {
+        System.out.println("Digite o CPF da pessoa que deseja consultar:");
+        String cpf = scanner.nextLine();
+
+        Pessoa pessoa = cpfToPessoaMap.get(cpf);
+
+        if (pessoa != null) {
+            System.out.println("Pessoa encontrada:");
+            System.out.println(pessoa);
+        } else {
+            System.out.println("CPF não encontrado no sistema.");
+        }
+    }
+
+    private static void listarPessoasCadastradas() {
+        if (pessoas.isEmpty()) {
+            System.out.println("Nenhuma pessoa cadastrada.");
+            return;
+        }
+
+        System.out.println("Pessoas cadastradas:");
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+    }
 }
--- Pessoa.java
+++ Pessoa.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Pessoa {
+class Pessoa {
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String sexo;
+
+    public Pessoa (String nome, String cpf, int idade, String sexo){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                '}';
+    }
 }
